e07410528b0f0469dd91217927c6bb5845bcac9b
Egor Danilov <mellowd94@gmail.com>
2023-08-28 20:50:37 +0200

    feat: add case"3"

--- Project3/src/Main.java
+++ Project3/src/Main.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-
     while (true) {
       EnumMainMenu menu = EnumMainMenu.readCommand(scanner);
 
@@ -16,6 +15,9 @@ public class Main {
           break;
         case SHOP:
           Shop.readCommand(scanner);
+          break;
+        case INFORMATION:
+          //Надо добавить
           break;
         case EXIT:
 
