1c3b1cf323038093bcc5195626f5ca48a3e682b2
Denys Kovtun <paryya28@gmail.com>
2023-08-28 20:02:13 +0200

    feat: create constructor and (geters, seters) in Buratino.java, Enemies.java

--- Project3/src/Buratino.java
+++ Project3/src/Buratino.java
@@ -1,2 +1,33 @@
 public class Buratino {
+
+    private String name;
+    private int health;
+    private int strength;
+
+
+    public Buratino(String name, int health, int strength) {
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
 }
