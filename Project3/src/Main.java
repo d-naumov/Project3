12ddb2093f111a08940e7a9161fe90de67d2da0c
Dimitri Naumov <nd76@gmx.de>
2023-08-28 20:14:08 +0200

    feat: add main Menu

--- Project3/src/Main.java
+++ Project3/src/Main.java
@@ -1,8 +1,26 @@
+import EnumPackage.MainMenu;
+import java.util.Scanner;
+
 public class Main {
-
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Hallo");
-    System.out.println();
+    while (true) {
+      MainMenu menu = MainMenu.readCommand(scanner);
+
+      switch (menu) {
+        case FIGHTING:
+          break;
+        case SHOP:
+          break;
+        case EXIT:
+
+          return;
+        default:
+          System.out.println("Некорректная команда");
+      }
+    }
   }
+
+
 }
