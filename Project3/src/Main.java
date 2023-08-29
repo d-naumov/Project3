83874e7a733b037583cda7d0f0eb9e3c0da23110
Natalja Röhm <roehm.natalia@gmail.com>
2023-08-29 20:33:39 +0200

    feat: add EnumStartMenu, in Main firstMenu

--- Project3/src/Main.java
+++ Project3/src/Main.java
@@ -1,11 +1,39 @@
 import EnumPackage.EnumMainMenu;
 import EnumPackage.EnumShop;
 
+import EnumPackage.EnumStartMenu;
 import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    startMenu();
+    System.out.println("--------------------------------------------------------------------");
+    System.out.println("Привет Буратино! Мы тебя так долго ждали, нам нужна твоя помощь. ");
+    System.out.println("  Хотим начать игру, где ты будешь главным героем.");
+    System.out.println("     Тебе нужно будет сразиться с неприятелем.");
+    System.out.println("     Победи и получишь \"Золотой ключик\".");
+    System.out.println("---------------------------------------------------------------------");
+    firstMenu();
+
+  }
+  public static void firstMenu(){
+    Scanner scanner = new Scanner(System.in);
+    boolean flag = true;
+    while (flag) {
+      EnumStartMenu menu = EnumStartMenu.readCommand(scanner);
+      switch (menu) {
+        case NEW_GAME:
+          System.out.println("Начинаем новую игру. Выберите ваши действия!");
+         startMenu();
+          break;
+        case LOAD_GAME:
+          // Загрузить игру
+          break;
+        case EXIT:
+          return;
+        default:
+          System.out.println("Некорректная команда");
+      }
+    }
   }
 
   public static void startMenu() {
@@ -28,7 +56,6 @@ public class Main {
           System.out.println(MainMenu.buratino.toString());
           break;
         case EXIT:
-
           return;
         default:
           System.out.println("Некорректная команда");
