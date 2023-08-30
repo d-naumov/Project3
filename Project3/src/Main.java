a1d0d0a34f2ec9b3f972cefa9bb56de7e52b9316
Natalja Röhm <roehm.natalia@gmail.com>
2023-08-30 17:24:34 +0200

    feat: remove EnumStartMenu.java and change in Main

--- Project3/src/Main.java
+++ Project3/src/Main.java
@@ -1,10 +1,10 @@
-import EnumPackage.EnumMainMenu;
-import EnumPackage.EnumShop;
-import EnumPackage.EnumStartMenu;
-
 import java.util.Scanner;
 
 public class Main {
+  public static final int NEW_GAME = 1;
+  public static final int LOAD_GAME = 2;
+  public static final int EXIT = 3;
+
   public static void main(String[] args) {
     StartNewGameMenu();
   }
@@ -12,7 +12,7 @@ public class Main {
   public static void StartNewGameMenu() {
     Scanner scanner = new Scanner(System.in);
     while (true) {
-      EnumStartMenu menu = EnumStartMenu.readCommand(scanner);
+      int menu = readCommand(scanner);
       switch (menu) {
         case NEW_GAME -> {
           System.out.println("Начинаем новую игру. Выберите ваши действия!");
@@ -20,13 +20,33 @@ public class Main {
           startMenu();
         }
         case LOAD_GAME -> {
+          // Load game logic
         }
-        // Загрузить игру
         case EXIT -> {
           return;
         }
         default -> System.out.println("Некорректная команда");
       }
+    }
+  }
+
+  public static int readCommand(Scanner scanner) {
+    printMenu();
+    System.out.println("Вы хотите начать игру сначала или загрузить ");
+    if (!scanner.hasNext()) {
+      throw new RuntimeException("Ожидается ввод команды");
+    }
+    String input = scanner.next();
+    scanner.nextLine();
+    switch (input.toLowerCase()) {
+      case "1":
+        return NEW_GAME;
+      case "2":
+        return LOAD_GAME;
+      case "3":
+        return EXIT;
+      default:
+        return -1; // Represents UNEXPECTED
     }
   }
 
@@ -39,28 +59,27 @@ public class Main {
     System.out.println("---------------------------------------------------------------------");
   }
 
-
-
   public static void startMenu() {
     Scanner scanner = new Scanner(System.in);
     boolean flag = true;
     while (flag) {
-      EnumMainMenu menu = EnumMainMenu.readCommand(scanner);
-
+      int menu = readCommand(scanner); // Assuming readCommand method works for startMenu too
       switch (menu) {
-        case FIGHTING -> MainMenu.enemiesInfo(scanner);
-        case SHOP -> {
-          EnumShop.readCommand(scanner);
-          Shop.selection(MainMenu.buratino, scanner);
+        case 1 -> MainMenu.enemiesInfo(scanner); // Assuming 1 corresponds to FIGHTING
+        case 2 -> {
+          // Shop logic
+          // Shop.selection(MainMenu.buratino, scanner);
         }
-        case INFORMATION ->
-          //Надо добавить
-            System.out.println(MainMenu.buratino.toString());
-        case EXIT -> {
+        case 3 -> {
           return;
         }
         default -> System.out.println("Некорректная команда");
       }
     }
+  }
+  public static void printMenu() {
+    System.out.println("1. Начать новую игру");
+    System.out.println("2. Загрузить игру");
+    System.out.println("3. Выход");
   }
 }
