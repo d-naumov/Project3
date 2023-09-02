420e6146d58b54661b9b81045b131ee26e14329b
Egor Danilov <mellowd94@gmail.com>
2023-09-02 09:05:52 +0200

    translate class ShopTest

--- Project3/Tests/ShopTest.java
+++ Project3/Tests/ShopTest.java
@@ -9,52 +9,52 @@ public class ShopTest {
   @Test
   public void testIncreaseHealthOnValidPurchase() {
     Buratino buratino = new Buratino("Buratino", 100, 20, 10);
-    String input = "1\n"; // Строка, которую пользователь вводит
+    String input = "1\n"; // The string the user enters
     Scanner scanner = new Scanner(new StringReader(input));
 
-    // Тестирование покупки предмета, который увеличивает здоровье
+    // Testing the purchase of an item that increases health
     Shop.selection(buratino, scanner);
 
-    // Проверка, что здоровье увеличилось на 20
+    // Checking that health has increased by 20
     assertEquals(120, buratino.getHealth());
   }
 
   @Test
   public void testIncreaseStrengthOnValidPurchase() {
     Buratino buratino = new Buratino("Buratino", 100, 20, 10);
-    String input = "2\n"; // Строка, которую пользователь вводит
+    String input = "2\n"; //  The string the user enters
     Scanner scanner = new Scanner(new StringReader(input));
 
-    // Тестирование покупки предмета, который увеличивает силу
+    // Testing the purchase of an item that increases strength
     Shop.selection(buratino, scanner);
 
-    // Проверка, что сила осталась неизменной (равной 20)
+    // Checking that the strength has remained unchanged (equal to 20)
     assertEquals(20, buratino.getStrength());
   }
 
   @Test
   public void testDecreaseMoneyOnValidPurchase() {
     Buratino buratino = new Buratino("Buratino", 100, 20, 20);
-    String input = "1\n"; // Строка, которую пользователь вводит
+    String input = "1\n"; // The string the user enters
     Scanner scanner = new Scanner(new StringReader(input));
 
-    // Тестирование уменьшения денег при покупке
+    // Testing money reduction when buying
     Shop.selection(buratino, scanner);
 
-    // Проверка, что деньги уменьшились на 10
+    // Checking if the money has decreased by 10
     assertEquals(10, buratino.getMoney());
   }
 
   @Test
   public void testInsufficientFunds() {
     Buratino buratino = new Buratino("Buratino", 180, 22, 10);
-    String input = "2\n"; // Строка, которую пользователь вводит
+    String input = "2\n"; // The string the user enters
     Scanner scanner = new Scanner(new StringReader(input));
 
-    // Тестирование покупки без достаточных средств
+    // Testing a purchase without enough funds
     Shop.selection(buratino, scanner);
 
-    // Проверка, что деньги не изменились
+    // Checking that the money has not changed
     assertEquals(10, buratino.getMoney());
   }
 
@@ -62,12 +62,12 @@ public class ShopTest {
   @Test
   public void testInvalidInput() {
     Buratino buratino = new Buratino("Buratino", 100, 20, 50);
-    String input = "invalid\n"; // Строка, которую пользователь вводит
+    String input = "invalid\n"; // The string the user enters
     Scanner scanner = new Scanner(new StringReader(input));
 
-    // Тестирование некорректного ввода данных пользователем
+    // Testing for invalid user input
     Shop.selection(buratino, scanner);
 
-    // Здесь нет утверждений (assertions), мы просто проверяем, что метод не вызывает исключение
+    // There are no assertions here, we just check that the method does not throw an exception
   }
 }
